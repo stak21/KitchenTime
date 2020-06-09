62e916f67e401629cb2d08da4780384f42ff3c2d
stak21 <stakashima21@gmail.com>
2020-06-09 00:25:21 -0700

    feat(app): Add functionality to delete items

--- app/shared/ListItem.js
+++ app/shared/ListItem.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
-import { TouchableOpacity, View, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components";
 
 const ListItem = ({ item, handleRemoveItem }) => (
-  <View>
+  <ItemContainer>
     <ItemText>{item}</ItemText>
     <TouchableOpacity onPress={() => handleRemoveItem(item)}>
-      <Text>Delete</Text>
+      <WarningText>Delete</WarningText>
     </TouchableOpacity>
-  </View>
+  </ItemContainer>
 );
 
 export default ListItem;
 
 const ItemText = styled.Text`
-  padding: 20px;
   font-size: 20px;
+`;
+
+const ItemContainer = styled.View`
+  padding: 20px;
   border: 1px solid black;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const WarningText = styled.Text`
+  color: white;
+  font-weight: 900;
+  background: #e54b4b;
+  border-radius: 5px;
+  border: none;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+  padding: 7.5px;
 `;
