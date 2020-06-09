4d815283609edda2ae50bf1fb215c083b556a6a7
stak21 <stakashima21@gmail.com>
2020-06-08 19:01:57 -0700

    feat(app): Initial searchbar

--- app/screens/ShoppingList.js
+++ app/screens/ShoppingList.js
@@ -1,23 +1,65 @@
 import React from "react";
-import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
+import styled from "styled-components/native";
+import { Text, View, Platform, StatusBar } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { TextInput } from "react-native-gesture-handler";
 
 const ShoppingList = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>ShoppingList</Text>
-    </View>
+    <Container>
+      <TitleContainer>
+        <TitleText>Shopping List</TitleText>
+      </TitleContainer>
+      <FlexContainer>
+        <SearchbarContainer>
+          <SearchBarTextArea>
+            <Icon name="search" style={{ fontSize: 24 }} />
+            <StyledTextInput placeholder="Search" />
+          </SearchBarTextArea>
+        </SearchbarContainer>
+      </FlexContainer>
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  padding-top: ${Platform.OS === "android" ? StatusBar.currentHeight : 0}px;
+`;
+
+const FlexContainer = styled.View`
+  flex: 1;
+`;
+
+const SearchbarContainer = styled.View`
+  background-color: orange;
+  height: 80px;
+  justify-content: center;
+  padding-horizontal: 5px;
+`;
+
+const SearchBarTextArea = styled.View`
+  height: 50px;
+  background-color: white;
+  flex-direction: row;
+  padding: 5px;
+  align-items: center;
+`;
+
+const StyledTextInput = styled.TextInput`
+  font-size: 24px;
+  margin-left: 15px;
+  width: 100%;
+`;
+
+const TitleContainer = styled.View`
+  align-items: center;
+  margin: 25px;
+`;
+const TitleText = styled.Text`
+  color: palevioletred;
+`;
 
 export default ShoppingList;
