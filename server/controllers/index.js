8c1dcee61a8bddb15bfde63675402cc0bf10b0ba
stak21 <stakashima21@gmail.com>
2020-06-11 09:28:23 -0700

    feat(server): Adds new routes and api for fetching recipes

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,7 @@
 const db = require("../db");
 const users = require("../models/users");
 const ingredients = require("../models/ingredients");
+const unirest = require("unirest");
 
 // User
 const createUser = (req, res) => {
@@ -54,10 +55,97 @@ const deleteIngredientToUser = (req, res) => {
     .catch((err) => res.status(404).send(err));
 };
 
+const getRecipesByIngredients = (req, res) => {
+  const { ingredients } = req.query;
+
+  console.log("ing" + ingredients);
+
+  var request = unirest(
+    "GET",
+    "https://webknox-recipes.p.rapidapi.com/recipes/findByIngredients"
+  );
+
+  request.query({
+    number: "5",
+    ingredients: ingredients,
+  });
+
+  request.headers({
+    "x-rapidapi-host": process.env.RAPIDAPI_HOST,
+    "x-rapidapi-key": process.env.RAPIDAPI_KEY,
+    useQueryString: true,
+  });
+
+  request.end(function (result) {
+    if (result.error) throw new Error(result.error);
+    res.status(200).send(result.body);
+  });
+};
+
+const searchRecipe = (req, res) => {
+  const { q } = req.query;
+
+  const request = unirest(
+    "GET",
+    "https://webknox-recipes.p.rapidapi.com/recipes/search"
+  );
+
+  request.query({
+    query: q,
+  });
+
+  request.headers({
+    "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+    "x-rapidapi-key": "f697106f5bmshab8fe07ae53f66ep1e22b9jsnba65f8b40899",
+    useQueryString: true,
+  });
+
+  request.end(function (result) {
+    if (result.error) throw new Error(result.error);
+
+    console.log(result.body);
+    // const title = q.toLowerCase();
+    // const body = result.body.results.filter((recipe) => {
+    //   return recipe.title.toLowerCase() === title;
+    // })[0];
+    const body = result.body.results[0];
+
+    res.status(200).json(body);
+  });
+};
+
+const searchRecipeByUrl = (req, res) => {
+  const { url } = req.query;
+
+  const request = unirest(
+    "GET",
+    "https://webknox-recipes.p.rapidapi.com/recipes/extract"
+  );
+
+  request.query({
+    url: url,
+  });
+
+  request.headers({
+    "x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+    "x-rapidapi-key": "f697106f5bmshab8fe07ae53f66ep1e22b9jsnba65f8b40899",
+    useQueryString: true,
+  });
+
+  request.end(function (result) {
+    if (result.error) throw new Error(result.error);
+
+    res.status(200).json(result.body);
+  });
+};
+
 module.exports = {
   createUser,
   createIngredient,
   getIngredient,
   addIngredientToUser,
   deleteIngredientToUser,
+  getRecipesByIngredients,
+  searchRecipe,
+  searchRecipeByUrl,
 };
--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ const port = 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+  console.log("incoming.... " + req.host);
+  next();
+});
+
+app.get("/api/recipes", controllers.getRecipesByIngredients);
+app.get("/api/recipe", controllers.searchRecipe);
+app.get("/api/recipe/url", controllers.searchRecipeByUrl);
 app.post("/api/users", controllers.createUser);
 app.post("/api/ingredients", controllers.createIngredient);
 app.get("/api/ingredients", controllers.getIngredient);
